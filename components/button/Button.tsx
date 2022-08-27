b7c2c5f86d3e1e61d31c7b886ab1c7d7bc182719
Andre Viallon <viallon.andre@gmail.com>
2022-08-27 13:17:22 +0200

    chore: add danger button type

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,6 +1,7 @@
 export enum ButtonType {
   PRIMARY = "primary",
   SECONDARY = "secondary",
+  DANGER = "danger",
 }
 
 interface ButtonProps {
@@ -11,7 +12,8 @@ interface ButtonProps {
 export const Button: React.FC<ButtonProps> = ({ buttonType, text }) => {
   const basicClasses = "px-4 py-2 rounded transition";
   const primaryClasses = "bg-blue-600 text-white hover:bg-blue-500";
-  const secondaryClasses = "bg-grey-100 text-black";
+  const secondaryClasses = "bg-gray-200 text-black hover:bg-gray-100";
+  const dangerClasses = "bg-red-600 text-white hover:bg-red-500";
 
   const getClasses = () => {
     let classes = basicClasses;
@@ -20,6 +22,8 @@ export const Button: React.FC<ButtonProps> = ({ buttonType, text }) => {
       classes += " " + primaryClasses;
     } else if (buttonType === ButtonType.SECONDARY) {
       classes += " " + secondaryClasses;
+    } else if (buttonType === ButtonType.DANGER) {
+      classes += " " + dangerClasses;
     }
 
     return classes;
