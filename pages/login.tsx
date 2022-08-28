4c46d3040825fc65e146269027b9c8abcd6436ff
Andre Viallon <viallon.andre@gmail.com>
2022-08-28 19:12:50 +0200

    chore: first take on login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,48 @@
-const login = () => {
-  return <div>login</div>;
+import { auth, googleAuthProvider } from "../util/firebase";
+import { Button, ButtonType } from "../components/button/Button";
+
+const Login = () => {
+  const user = false;
+
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+  const loginButtons = () => (
+    <div className="flex flex-col items-center justify-center gap-2">
+      <Button
+        buttonType={ButtonType.SECONDARY}
+        text="Sign in with Google"
+        onClick={signInWithGoogle}
+      ></Button>
+      <Button
+        buttonType={ButtonType.PRIMARY}
+        text="Sign in anonymously"
+      ></Button>
+    </div>
+  );
+
+  const logoutButton = () => (
+    <Button
+      buttonType={ButtonType.DANGER}
+      text="Logout"
+      onClick={logout}
+    ></Button>
+  );
+
+  return (
+    <div className="flex justify-center mx-auto mt-8">
+      {user ? logoutButton() : loginButtons()}
+    </div>
+  );
 };
 
 export default login;
